8356bb351ec976f2c65c95b4e1ad387c783da205
Spencer Fulgham <spencerman@comcast.net>
2021-04-22 14:59:52 -0700

    basic color testing

--- AppLayout/static/js/theme_change.js
+++ AppLayout/static/js/theme_change.js
@@ -1,31 +1,15 @@
  // Function to change webpage background color
- function countryTheme(){
-    document.getElementById("friends").style.background = '#fffdd0';
-    document.getElementById("friendonlinetext").style.color = '#420d09';
-    for(let i=0; i<=4; i++){
-        document.getElementById("friendtile"+i).style.background = '#997950';
-    }
-    for(let i=1; i<=3; i++){
-        document.getElementById("p"+i).style.background = '#f8e473';
-        document.getElementById("t"+i).style.color = '#a45a52';
-        document.getElementById("b"+i).style.color = '#a45a52';
-    }
-    document.body.style.background = '#420d09';
-}
+ function defaultTheme(){
+    document.getElementById("friends").style.background = '#89cfef';
+    document.getElementById("friendonlinetext").style.color = '#1c2951';
+    document.body.style.background = '#cdb7f6';
 
-// Function to change heading background color
-function rapTheme(){
-    document.getElementById("friends").style.background = '#191414';
-    document.getElementById("friendonlinetext").style.color = '#d9dddc';
-    for(let i=0; i<=4; i++){
-        document.getElementById("friendtile"+i).style.background = '#800000';
-    }
+    for(let i=0; i<=4; i++){ document.getElementById("friendtile"+i).style.background = '#d0f0c0'; }
     for(let i=1; i<=3; i++){
-        document.getElementById("p"+i).style.background = '#800000';
-        document.getElementById("t"+i).style.color = '#d9dddc';
-        document.getElementById("b"+i).style.color = '#d9dddc';
+        document.getElementById("p"+i).style.background = '#89cfef';
+        document.getElementById("t"+i).style.color = '#d0f0c0';
+        document.getElementById("b"+i).style.color = '#d0f0c0';
     }
-    document.body.style.background = '#777b7e';
 }
 
 function spotifyTheme(){
@@ -40,6 +24,34 @@ function spotifyTheme(){
         document.getElementById("b"+i).style.color = '#1db954';
     }
     document.body.style.background = 'black';
+}
+ 
+ function countryTheme(){
+    document.getElementById("friends").style.background = '#fffdd0';
+    document.getElementById("friendonlinetext").style.color = '#420d09';
+    for(let i=0; i<=4; i++){
+        document.getElementById("friendtile"+i).style.background = '#997950';
+    }
+    for(let i=1; i<=3; i++){
+        document.getElementById("p"+i).style.background = '#f8e473';
+        document.getElementById("t"+i).style.color = '#a45a52';
+        document.getElementById("b"+i).style.color = '#a45a52';
+    }
+    document.body.style.background = '#420d09';
+}
+
+function rapTheme(){
+    document.getElementById("friends").style.background = '#191414';
+    document.getElementById("friendonlinetext").style.color = '#d9dddc';
+    for(let i=0; i<=4; i++){
+        document.getElementById("friendtile"+i).style.background = '#800000';
+    }
+    for(let i=1; i<=3; i++){
+        document.getElementById("p"+i).style.background = '#800000';
+        document.getElementById("t"+i).style.color = '#d9dddc';
+        document.getElementById("b"+i).style.color = '#d9dddc';
+    }
+    document.body.style.background = '#777b7e';
 }
 
 function popTheme(){
