6d733dc7666b3c0c4a930ef83c29c21a89932ac0
Ash Shams <ashyshams@outlook.com>
2021-04-27 19:07:59 -0700

    Merge 4/27 Metal Theme + Kaitlyn + Hannah

--- AppLayout/models.py
+++ AppLayout/models.py
@@ -13,11 +13,20 @@ from pydal.validators import *
 #
 # db.commit()
 #
+
+def empty_albums():
+    bannerList = []
+    for i in range(12):
+        bannerList.append("x")
+    return bannerList
+
 db.define_table(
     'dbUser',
     Field('userID', notnull=True, unique=True),
     Field('display_name'),
+    Field('chosen_theme'),
     #Ash: email may be unnecessary. 
+    Field('bio_status'),
     Field('profile_pic'),
 )
 
@@ -26,6 +35,7 @@ db.define_table(
     Field('userID', notnull=True),
     Field('display_name'),
     Field('profile_pic'),
+    Field('bio_status'),
     #Ash: This should connect the friends table to the user table
     Field('friendToWhoID', db.dbUser)
 )
@@ -46,6 +56,35 @@ db.define_table(
     Field('topTracksOfWho', db.dbUser)
 )
 
+db.define_table(
+    'mediumTerm',
+    Field('topTracks', 'list:string'),
+    Field('topArtists', 'list:string'), # Artists for each top track
+    Field('imgList', 'list:string'),
+    Field('trackLinks', 'list:string'),
+    Field('artistLinks', 'list:string'), 
+    Field('topTracksOfWho', db.dbUser)
+)
+
+db.define_table(
+    'longTerm',
+    Field('topTracks', 'list:string'),
+    Field('topArtists', 'list:string'), # Artists for each top track
+    Field('imgList', 'list:string'),
+    Field('trackLinks', 'list:string'),
+    Field('artistLinks', 'list:string'), 
+    Field('topTracksOfWho', db.dbUser)
+)
+
+
+db.define_table(
+    'squares',
+    #Change this to one big field with 1 list with 6 index [0][1]...
+    #Also just return the albumsOfWho ID for albumInput because it should be unique. 
+    Field('coverList', 'list:string', default=empty_albums()), 
+    Field('urlList', 'list:string', default=empty_albums()),
+    Field('albumsOfWho', db.dbUser)
+)
 
 #“extra” is not a keyword; it’s a custom attribute now attached to the field object. You can do it with tables too but they must be preceded by an underscore to avoid naming conflicts with fields:
 #db.table._extra = {}
