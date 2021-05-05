69c06cbc52db4a326e3dbef788f6e22616b6a573
Ash Shams <ashyshams@outlook.com>
2021-05-04 22:11:58 -0700

    Merge 5/4 Sprint 2 End. Only Kaitlyn + Ash Merged

--- AppLayout/static/js/user.js
+++ AppLayout/static/js/user.js
@@ -12,6 +12,18 @@ let init = (app) => {
         isEditingBio: 0,
         bio: "",
         originalBio: "",
+        //To display in top songs dropdown
+        termString: "",
+        //1 is short term, 2 is medium term, 3 is long term
+        currentTerm: 0,
+        topTracks: [],
+        topArtists: [],
+        imgList: [],
+        trackLinks: [],
+        artistLinks: [],
+        playlistNames: [],
+        playlistImages: [],
+        playlistLinks: []
     };
     
     //Used to save the album cover art for the user profiles. 
@@ -50,13 +62,60 @@ let init = (app) => {
             app.data.isEditingAlbums = 0;
         }
     };
+
+    //Called to see the top 10 songs
+    app.seeTerm = () => {
+        console.log("I'm in seeTerm");
+        axios.get(getTopSongs).then((result) => {
+            app.data.termString = result.data.term_str;
+            app.data.topTracks = result.data.topTracks;
+            app.data.topArtists = result.data.topArtists;
+            app.data.imgList = result.data.imgList;
+            app.data.trackLinks = result.data.trackLinks;
+            app.data.artistLinks = result.data.artistLinks;
+            }).then(() => {
+                //show what the bio is.
+                console.log(app.data.termString);
+            });
+        }
     
+    //Called to change the term of top 10 songs
+    app.changeTerm = (term) => {
+        console.log("I'm in changeTerm");
+        axios.post(getTopSongs, null, {params: {
+            term: term,
+            }}).then(() => {
+                app.seeTerm(); 
+            }).catch(() => {
+                console.log("Caught error");
+            });
+        };
+
+    /*
+    app.seePlaylists = () => {
+        console.log("I'm in seePlaylists");
+        axios.get(getPlaylists).then((result) => {
+            let bigList = result.data.bigList;
+            app.data.playlistNames = bigList[0];
+            app.data.playlistImages = bigList[1];
+            app.data.playlistLinks = bigList[2];
+            }).then(() => {
+                //show what the bio is.
+                console.log("see playlists success");
+            });
+        };
+    */
+
+
     // We form the dictionary of all methods, so we can assign them
     // to the Vue app in a single blow.
     app.methods = {
         controlEditButton: app.controlEditButton,
         save_bio: app.save_bio,
-        cancel_bio: app.cancel_bio
+        cancel_bio: app.cancel_bio,
+        seeTerm: app.seeTerm,
+        changeTerm: app.changeTerm,
+        //seePlaylists: app.seePlaylists
     };
 
     // This creates the Vue instance.
@@ -68,14 +127,19 @@ let init = (app) => {
 
     // And this initializes it.
     app.init = () => {
+
         axios.get(userBio).then((result) => {
             let bio = result.data.userBio;
             app.data.bio = bio;
             app.data.originalBio = bio;
             }).then(() => {
-                console.log("Begin");
                 console.log(app.data.bio);
+                //After setting the Bio move on to displaying top songs
+                //and user playlists
+                app.seeTerm();
+                //app.seePlaylists();
             });
+
     };
 
     // Call to the initializer.
